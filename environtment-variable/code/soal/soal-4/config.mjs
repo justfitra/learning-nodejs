5ffe789ee0ad19c44703cd5e178294d65f852cbb
PitoyoAidropHunter <pitoyoaidrophunter@gmail.com>
2025-10-28 00:29:42 +0700

    fix code : config.mjs

--- environtment-variable/code/soal/soal-4/config.mjs
+++ environtment-variable/code/soal/soal-4/config.mjs
@@ -2,6 +2,17 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
+const requiredEnv = ["PORT", "DB_HOST", "DB_USER", "DB_PASS"];
+
+requiredEnv.forEach((key) => {
+  if (!process.env[key]) {
+    console.error(`Missing environment variable: ${key}`);
+    process.exit(1);
+  }
+});
+
+console.log("All environment variables are set!");
+
 export const config = {
   host: process.env.HOST,
   port: process.env.PORT,
