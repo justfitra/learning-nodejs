ed0fedf82ef658e75cf72b2e0bfbcf9b9d233008
PitoyoAidropHunter <pitoyoaidrophunter@gmail.com>
2025-12-18 13:55:00 +0700

    fix this code

--- authentication_and_security/code/materi/bycrpt_password_hashing/src/utils/password.js
+++ authentication_and_security/code/materi/bycrpt_password_hashing/src/utils/password.js
@@ -1,6 +1,7 @@
 import bcrypt from "bcrypt";
+const saltRounds = 10;
 export const hashPassword = async (password) => {
-  return await bcrypt.hash(password, 10);
+  return await bcrypt.hash(password, saltRounds);
 };
 export const comparePassword = async (plain, hash) => {
   return await bcrypt.compare(plain, hash);
