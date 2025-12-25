718bd3bc959e560be534e489c54f2a2422a7f0d8
PitoyoAidropHunter <pitoyoaidrophunter@gmail.com>
2025-12-25 19:49:50 +0700

    add new lesson : helmet and rate limiter

--- authentication_and_security/code/materi/cors_helmet_rate_limiting/src/app.js
+++ authentication_and_security/code/materi/cors_helmet_rate_limiting/src/app.js
@@ -3,13 +3,20 @@ import cors from "cors";
 import { envConfig } from "./config/envConfig.js";
 import { errorHandler } from "./middlewares/errorHandler.js";
 import router from "./routes/userRouter.js";
+import helmet from "helmet";
+import rateLimit from "express-rate-limit";
 
 const app = express();
-
-// const corsOption = ;
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  limit: 100,
+  standardHeaders: "draft-8",
+  message: "Too many requests, try again later",
+});
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(helmet());
 app.use(
   cors({
     origin: [`http://${envConfig.app_host}:${envConfig.app_port}`],
@@ -17,6 +24,7 @@ app.use(
     credentials: true,
   })
 );
+app.use(limiter);
 
 app.use("/api/users", router);
 app.use(errorHandler);
