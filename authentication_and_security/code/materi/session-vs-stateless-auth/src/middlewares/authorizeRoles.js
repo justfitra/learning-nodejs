d4ed3adf87f29bf962d4e59a4159119869a40a13
PitoyoAidropHunter <pitoyoaidrophunter@gmail.com>
2026-01-28 17:45:05 +0700

    update middleware authorize-role

--- authentication_and_security/code/materi/session-vs-stateless-auth/src/middlewares/authorizeRoles.js
+++ authentication_and_security/code/materi/session-vs-stateless-auth/src/middlewares/authorizeRoles.js
@@ -3,7 +3,7 @@ import { AppError } from "../utils/appError.js";
 export const authorization =
   (...allowedRoles) =>
   (req, res, next) => {
-    if (!allowedRoles.includes(req.user.role)) {
+    if (!allowedRoles.includes(req.session.user.role)) {
       throw new AppError("Forbidden", 403);
     }
 
