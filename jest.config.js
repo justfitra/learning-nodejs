0309df749b2e4d2a4d3298a31a9e0e0db4de0057
PitoyoAidropHunter <pitoyoaidrophunter@gmail.com>
2026-05-21 11:39:55 +0700

    add : (jest-config)

---
+++ jest.config.js
@@ -0,0 +1,7 @@
+// jest.config.js
+export default {
+  globalSetup:
+    "./software-architecture&advanced-concepts/code/materi/unit-testing/jest.setup.js",
+  transform: {},
+  testTimeout: 5000, // ⬅️ load .env sebelum apapun dijalankan
+};
