b55bcd4bbb8b2b47c2c78fa900081ca2f27c7642
PitoyoAidropHunter <pitoyoaidrophunter@gmail.com>
2026-09-12 14:15:30 +0700

    update lesson : deployment

--- deployment-profiling-monitoring/code/materi/management-dev-staging-prod/src/config/db.js
+++ deployment-profiling-monitoring/code/materi/management-dev-staging-prod/src/config/db.js
@@ -3,8 +3,7 @@ import { envConfig } from "./envConfig.js";
 
 export const connectDB = async () => {
   try {
-    await mongoose.connect(`${envConfig.db_host}/${envConfig.db_name}`);
-
+    await mongoose.connect(envConfig.mongo_uri);
     console.log(`Database connected: ${envConfig.db_name} `);
   } catch (err) {
     console.error(err.message);
