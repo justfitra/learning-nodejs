206ad9eaaeedd22cf384a3d62cb822cc8d8944be
PitoyoAidropHunter <pitoyoaidrophunter@gmail.com>
2026-09-13 15:01:27 +0700

    update lesson : deploy

--- deployment-profiling-monitoring/code/materi/deployment/src/config/couldinary.js
+++ deployment-profiling-monitoring/code/materi/deployment/src/config/couldinary.js
@@ -2,9 +2,9 @@ import { v2 as cloudinary } from "cloudinary";
 import { envConfig } from "./envConfig.js";
 
 cloudinary.config({
-  cloud_name: envConfig.cluodinary_api_key,
-  api_key: envConfig.cluodinary_api_key,
-  api_secret: envConfig.cluodinary_api_key,
+  cloud_name: envConfig.cloudinary_cloud_name,
+  api_key: envConfig.cloudinary_api_key,
+  api_secret: envConfig.cloudinary_api_secret,
 });
 
 export default cloudinary;
